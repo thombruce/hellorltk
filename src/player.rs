369930a6e2fc7a982dee231223ec0a894f7d6b72
Thom Bruce <thom@thombruce.com>
2022-06-05 04:22:21 +0100

    feat: add foundation for player attack

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
-use super::{Map, Player, Position, RunState, State, Viewshed};
-use rltk::{Point, Rltk, VirtualKeyCode};
+use super::{Map, Player, Position, RunState, State, Viewshed, CombatStats};
+use rltk::{Point, Rltk, VirtualKeyCode, console};
 use specs::prelude::*;
 use std::cmp::{max, min};
 
@@ -7,10 +7,24 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
     let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let combat_stats = ecs.read_storage::<CombatStats>();
     let map = ecs.fetch::<Map>();
 
     for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
         let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+
+        for potential_target in map.tile_content[destination_idx].iter() {
+            let target = combat_stats.get(*potential_target);
+            match target {
+                None => {}
+                Some(_t) => {
+                    // Attack it
+                    console::log("From Hell's Heart, I stab thee!");
+                    return; // So we don't move after attacking
+                }
+            }
+        }
+
         if !map.blocked[destination_idx] {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
