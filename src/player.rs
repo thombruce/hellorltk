04941697fe21a5d23270f791061cfc1010ca477e
Thom Bruce <thom@thombruce.com>
2022-06-05 02:43:25 +0100

    feat: add diagonal movements

--- src/player.rs
+++ src/player.rs
@@ -45,6 +45,15 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
                 try_move_player(0, 1, &mut gs.ecs)
             }
 
+            // Diagonals
+            VirtualKeyCode::Numpad7 | VirtualKeyCode::Y => try_move_player(-1, -1, &mut gs.ecs),
+
+            VirtualKeyCode::Numpad9 | VirtualKeyCode::U => try_move_player(1, -1, &mut gs.ecs),
+
+            VirtualKeyCode::Numpad3 | VirtualKeyCode::N => try_move_player(1, 1, &mut gs.ecs),
+
+            VirtualKeyCode::Numpad1 | VirtualKeyCode::B => try_move_player(-1, 1, &mut gs.ecs),
+
             _ => return RunState::Paused,
         },
     }
