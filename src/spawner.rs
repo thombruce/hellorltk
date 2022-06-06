aef5b745146f4c2005b813f5403620a752248d80
Thom Bruce <thom@thombruce.com>
2022-06-06 23:26:32 +0100

    feat: add health potion entities

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,10 +1,12 @@
 use super::{
-    BlocksTile, CombatStats, Monster, Name, Player, Position, Rect, Renderable, Viewshed, MAPWIDTH,
+    BlocksTile, CombatStats, Item, Monster, Name, Player, Position, Potion, Rect, Renderable,
+    Viewshed, MAPWIDTH,
 };
 use rltk::{RandomNumberGenerator, RGB};
 use specs::prelude::*;
 
 const MAX_MONSTERS: i32 = 4;
+const MAX_ITEMS: i32 = 2;
 
 /// Spawns the player and returns his/her entity object.
 pub fn player(ecs: &mut World, player_x: i32, player_y: i32) -> Entity {
@@ -52,11 +54,13 @@ pub fn random_monster(ecs: &mut World, x: i32, y: i32) {
 /// Fills a room with stuff!
 pub fn spawn_room(ecs: &mut World, room: &Rect) {
     let mut monster_spawn_points: Vec<usize> = Vec::new();
+    let mut item_spawn_points: Vec<usize> = Vec::new();
 
     // Scope to keep the borrow checker happy
     {
         let mut rng = ecs.write_resource::<RandomNumberGenerator>();
         let num_monsters = rng.roll_dice(1, MAX_MONSTERS + 2) - 3;
+        let num_items = rng.roll_dice(1, MAX_ITEMS + 2) - 3;
 
         for _i in 0..num_monsters {
             let mut added = false;
@@ -70,6 +74,19 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
                 }
             }
         }
+
+        for _i in 0..num_items {
+            let mut added = false;
+            while !added {
+                let x = (room.x1 + rng.roll_dice(1, i32::abs(room.x2 - room.x1))) as usize;
+                let y = (room.y1 + rng.roll_dice(1, i32::abs(room.y2 - room.y1))) as usize;
+                let idx = (y * MAPWIDTH) + x;
+                if !item_spawn_points.contains(&idx) {
+                    item_spawn_points.push(idx);
+                    added = true;
+                }
+            }
+        }
     }
 
     // Actually spawn the monsters
@@ -77,6 +94,13 @@ pub fn spawn_room(ecs: &mut World, room: &Rect) {
         let x = *idx % MAPWIDTH;
         let y = *idx / MAPWIDTH;
         random_monster(ecs, x as i32, y as i32);
+    }
+
+    // Actually spawn the potions
+    for idx in item_spawn_points.iter() {
+        let x = *idx % MAPWIDTH;
+        let y = *idx / MAPWIDTH;
+        health_potion(ecs, x as i32, y as i32);
     }
 }
 
@@ -111,5 +135,21 @@ fn monster<S: ToString>(ecs: &mut World, x: i32, y: i32, glyph: rltk::FontCharTy
             defense: 1,
             power: 4,
         })
+        .build();
+}
+
+fn health_potion(ecs: &mut World, x: i32, y: i32) {
+    ecs.create_entity()
+        .with(Position { x, y })
+        .with(Renderable {
+            glyph: rltk::to_cp437('¡'),
+            fg: RGB::named(rltk::MAGENTA),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .with(Name {
+            name: "Health Potion".to_string(),
+        })
+        .with(Item {})
+        .with(Potion { heal_amount: 8 })
         .build();
 }
