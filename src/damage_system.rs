24d66ea072ca1cfa1c35171f6d7ee3b89add9e59
Thom Bruce <thom@thombruce.com>
2022-06-13 02:46:00 +0100

    feat: add Game Over screen

--- src/damage_system.rs
+++ src/damage_system.rs
@@ -1,4 +1,4 @@
-use super::{gamelog::GameLog, CombatStats, Name, Player, SufferDamage};
+use super::{gamelog::GameLog, CombatStats, Name, Player, RunState, SufferDamage};
 use specs::prelude::*;
 
 pub struct DamageSystem {}
@@ -40,7 +40,10 @@ pub fn delete_the_dead(ecs: &mut World) {
                         }
                         dead.push(entity)
                     }
-                    Some(_) => log.entries.push("You are dead".to_string()),
+                    Some(_) => {
+                        let mut runstate = ecs.write_resource::<RunState>();
+                        *runstate = RunState::GameOver;
+                    }
                 }
             }
         }
